cfe3719a6366285886e020d95ac6bd18ba36c646
HyderYash <hrkabirsuri@gmail.com>
2025-06-14 22:33:59 +0530

    ad push

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,9 +20,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} bg-slate-950 min-h-screen flex items-center justify-center`}>
         {/* Responsive Mobile Container */}
-        <div className="relative w-full max-w-[430px] h-screen min-h-screen bg-slate-900 shadow-2xl overflow-hidden flex flex-col">
-          <div className="flex-1 flex flex-col overflow-hidden">
-
+        <div className="relative w-full max-w-[430px] bg-slate-900 shadow-2xl overflow-hidden flex flex-col mx-auto">
+          <div className="flex-1 flex flex-col">
             <CoinProvider>
               <div className="flex-1 ">
                 {children}
